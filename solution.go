8764a41fd4e6f87c88b58adb37af175c4ead32bb
Altynbek <altuxadark98@gmail.com>
2022-02-22 20:15:33 +0600

    go

--- solution.go
+++ solution.go
@@ -8,19 +8,21 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-type Aboba struct {
-	SidesTriangle int
-	SidesSquare   int
-	SidesCircle   int
-}
+// type Aboba struct {
+// 	SidesTriangle int
+// 	SidesSquare   int
+// 	SidesCircle   int
+// }
 
-func CalcSquare(sideLen float64, sidesNum Aboba) float64 {
-	if sidesNum.SidesSquare == 4 {
+type intCustomType int
+
+func CalcSquare(sideLen float64, sidesNum intCustomType) float64 {
+	if sidesNum == 4 {
 		return sideLen * sideLen
-	} else if sidesNum.SidesTriangle == 3 {
+	} else if sidesNum == 3 {
 		a := sideLen * sideLen * 1.73205
 		return a / 4
-	} else if sidesNum.SidesCircle == 0 {
+	} else if sidesNum == 0 {
 		return sideLen * sideLen * 3.14159265358979323846264338327950288419716939937510582097494459
 	} else {
 		return 0
