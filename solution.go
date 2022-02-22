1733b56d5663fa9ca915d327ab5caf2e0b97d879
Altynbek <altuxadark98@gmail.com>
2022-02-22 19:52:17 +0600

    go

--- solution.go
+++ solution.go
@@ -8,5 +8,16 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+func CalcSquare(sideLen float64, sidesNum int) float64 {
+	if sidesNum == 4 {
+		return sideLen * sideLen
+	} else if sidesNum == 3 {
+		a := sideLen * sideLen * 1.73205
+		return a / 4
+	} else if sidesNum == 0 {
+		return sideLen * sideLen * 3.14159265358979323846264338327950288419716939937510582097494459
+	} else {
+		return 0
+	}
+	return 0
 }
